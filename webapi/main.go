135ed542e69d66a670bd343cdfb15c6cc4b81fbe
Oscar <oscar.f.larsson@gmail.com>
2020-08-11 15:06:23 +0200

    Websocket up and running

--- webapi/main.go
+++ webapi/main.go
@@ -1,18 +1,30 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"net/http"
 )
 
-func init() {
-
-}
+var addr = flag.String("addr", "127.0.0.1:8080", "http service address")
 
 func main() {
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
 
-	error := http.ListenAndServe("3001", nil)
-	if error != nil {
-		fmt.Printf("Hello")
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+
+	log.Println("Web api started, listening on ws://127.0.0.1:8080")
+
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
