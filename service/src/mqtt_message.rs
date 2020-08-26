e6076708b8a06c65d59d19fba804b0cad1938c2a
Oscar <oscar.f.larsson@gmail.com>
2020-08-26 15:12:09 +0200

    Changed hashset from datetime to guid instead

--- service/src/mqtt_message.rs
+++ service/src/mqtt_message.rs
@@ -1,4 +1,3 @@
-use chrono::{NaiveDateTime};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -11,8 +10,7 @@ pub struct RoundTrip {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "PascalCase")]
 pub struct Batch {
-    #[serde(with = "custom_date_format")]
-    timestamp: NaiveDateTime,
+    hash_key: String,
     key: String,
     value: String,
 }
@@ -39,41 +37,7 @@ macro_rules! impl_KeyValue {
 impl_KeyValue!(for Batch, RoundTrip);
 
 impl Batch {
-    pub fn timestamp(&self) -> &NaiveDateTime {
-        return &self.timestamp;
-    }
-}
-
-mod custom_date_format {
-    use chrono::{Utc, NaiveDateTime, DateTime};
-    use serde::{self, Deserialize, Deserializer, Serializer, de::Error};
-
-    // The signature of a serialize_with function must follow the pattern:
-    //
-    //    fn serialize<S>(&T, S) -> Result<S::Ok, S::Error>
-    //    where
-    //        S: Serializer
-    //
-    // although it may also be generic over the input types T.
-    pub fn serialize<S>(date: &NaiveDateTime, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let s = DateTime::<Utc>::from_utc(*date, Utc).to_rfc3339();
-        serializer.serialize_str(&s)
-    }
-    // The signature of a deserialize_with function must follow the pattern:
-    //
-    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
-    //    where
-    //        D: Deserializer<'de>
-    //
-    // although it may also be generic over the output types T.
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDateTime, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        Ok(DateTime::parse_from_rfc3339(&s).map_err(D::Error::custom)?.naive_utc())
+    pub fn hash_key(&self) -> &String {
+        return &self.hash_key;
     }
 }
