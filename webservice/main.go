1d7aad1c0e4c48254c3a1f53be28ed827a19ef1a
Oscar <oscar.f.larsson@gmail.com>
2020-08-28 20:46:59 +0200

    Removed batch publish from Go webservice since it reside in another service now

--- webservice/main.go
+++ webservice/main.go
@@ -9,14 +9,12 @@ import (
 	"strconv"
 	"webservice/mqtt"
 
-	"github.com/google/uuid"
 	"github.com/streadway/amqp"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
 var exchangeName = flag.String("exchange", "exchange_events", "Name for MQTT exchange")
 var roundTripQueueName = flag.String("roundTripQueue", "events", "Name of the MQTT queue to publish roundtrip messages to")
-var batchQueueName = flag.String("batchQueue", "batch", "Name of the MQTT queue to publish roundtrip messages to")
 var connectionName = flag.String("connection", "event_producer", "Name of the MQTT connection")
 var batchSize = 100
 
@@ -49,18 +47,7 @@ func main() {
 		}
 	})
 
-	// Will publish a batch of key/value pairs to MQTT broker, won't do a roundtrip, but will
-	// wait for confirmation of all messages and will be saved in Redis.
-	http.HandleFunc("/api/batch", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Got a request to publish batch. Size of batch: ", batchSize)
-		if succeded := publishBatch(connection); succeded == true {
-			w.WriteHeader(http.StatusOK)
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	})
-
-	log.Println("Webservice started, listening on http://localhost:8080")
+	log.Println("Webservice started, listening on http://+:8080")
 
 	err = http.ListenAndServe(*addr, enableCors(http.DefaultServeMux))
 	if err != nil {
@@ -86,7 +73,6 @@ func initMQTTConnection() *mqtt.Connection {
 		*exchangeName,   // exchange name
 		[]string{
 			*roundTripQueueName, // queues
-			*batchQueueName,
 		})
 
 	if err := connection.Connect(); err != nil {
@@ -117,35 +103,5 @@ func publish(connection *mqtt.Connection, message RoundTrip) bool {
 	}
 
 	log.Printf("Successfully published message with key: %s and value: %s!", message.Key, message.Value)
-	return true
-}
-
-func publishBatch(connection *mqtt.Connection) bool {
-	mqttMessages := []amqp.Publishing{}
-	i := 0
-	batchKey := uuid.New().String()
-	for i < batchSize {
-		message := BatchModel{
-			HashKey: batchKey,
-			Key:     batchKey + "_key_" + strconv.Itoa(i),
-			Value:   batchKey + "_value_" + strconv.Itoa(i),
-		}
-
-		mqttMessage := amqp.Publishing{
-			ContentType:  "application/json",
-			DeliveryMode: amqp.Transient,
-			Body:         message.ConvertToByteArray(),
-		}
-
-		mqttMessages = append(mqttMessages, mqttMessage)
-		i++
-	}
-
-	if err := connection.PublishBatch(mqttMessages, *batchQueueName); err != nil {
-		log.Fatalf("Failed to publish batch, err: %s!", err.Error())
-		return false
-	}
-
-	log.Printf("Successfully published batch of (%d) messages!", batchSize)
 	return true
 }
