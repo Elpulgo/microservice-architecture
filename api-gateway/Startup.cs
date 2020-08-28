db562006dae4fa403b8b29795e1029fd322ee09c
Oscar <elpulgo@users.noreply.github.com>
2020-08-28 21:07:16 +0200

    Added api-gateway with Ocelot. Now rerouting, batch-webservice, webservice and websocketservice under one umbrella, and only need to expose one endpoint to client-side

---
+++ api-gateway/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace api_gateway
+{
+    public class Startup
+    {
+        private readonly string CorsPolicyName = "AllowOrigin";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(c => c.AddPolicy(
+               CorsPolicyName,
+               options => options.AllowAnyOrigin().AllowAnyHeader()));
+
+            services.AddOcelot();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseWebSockets();
+            app.UseOcelot().Wait();
+            app.UseRouting();
+            app.UseCors(CorsPolicyName);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
