22d345193595f178169b2c973755e5dbfb77b9cd
Oscar <oscar.f.larsson@gmail.com>
2020-09-06 12:44:29 +0200

    Aligned batch model in batch-service with model in redis-service, ontrudiced isLastInBatch and batchSize. Improved logging, and changed batch publish loop to simple for loop instead of foreach

--- batch-service/Models/Batch.cs
+++ batch-service/Models/Batch.cs
@@ -9,15 +9,25 @@ namespace batch_webservice
 
         }
 
-        public Batch(Guid hashKey, string key, string value)
+        public Batch(
+            Guid hashKey,
+            string key,
+            string value,
+            int batchSize,
+            bool isLastInBatch = false)
         {
             HashKey = hashKey;
             Key = key;
             Value = value;
+            BatchSize = batchSize;
+            IsLastInBatch = isLastInBatch;
         }
 
         public Guid HashKey { get; set; }
         public string Key { get; set; }
         public string Value { get; set; }
+        public int BatchSize { get; set; }
+
+        public bool IsLastInBatch { get; set; }
     }
 }
