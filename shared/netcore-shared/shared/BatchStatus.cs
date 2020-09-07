a567b7589bac2358485e10b961ad84420509102e
Oscar <oscar.f.larsson@gmail.com>
2020-09-07 20:07:23 +0200

    Added shared dotnet core library for BatchStatus to be used by Batch service and Client

---
+++ shared/netcore-shared/shared/BatchStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetCoreShared
+{
+    public enum BatchStatus
+    {
+        None = 0,
+        PendingConsume = 1,
+        PendingDatabase = 2,
+        DatabaseOperationFailed = 3,
+        Invalid = 4,
+        TimeoutExceeded = 5,
+        Done = 6,
+    }
+}
